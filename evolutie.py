b9cf8c2c99ff559af8e06432c50cb47070b0df98
Iosif Vieru <iosifvieru440@gmail.com>
2025-01-08 22:37:26 +0200

    normalizarea datelor

--- evolutie.py
+++ evolutie.py
@@ -1,7 +1,6 @@
 from ucimlrepo import fetch_ucirepo 
 
-  
-#incarcarea datelor
+# incarcarea datelor
 iris = fetch_ucirepo(id=53) 
 
 X = iris.data.features 
@@ -10,4 +9,11 @@ y = iris.data.targets
 print(X)
 print(y)
 
+# normalizarea datelor
+X_min = X.min(axis=0)
+X_max = X.max(axis=0)
 
+X_normalized = (X - X_min) / (X_max - X_min)
+
+print("Date normalizate:")
+print(X_normalized)
