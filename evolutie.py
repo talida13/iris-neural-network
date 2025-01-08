d20470fe75d4f5f711a2d6fe6ccb5936930d7765
mateispic <matei-ioan.pricop@student.tuiasi.ro>
2025-01-08 22:39:37 +0200

    impartirea datelor

--- evolutie.py
+++ evolutie.py
@@ -17,3 +17,8 @@ X_normalized = (X - X_min) / (X_max - X_min)
 
 print("Date normalizate:")
 print(X_normalized)
+
+#impartirea datelor in set de antrenare si testare
+train_size = int(0.8 * len(X))
+X_train, X_test = X[:train_size], X[train_size:]
+y_train, y_test = y[:train_size], y[train_size:]
